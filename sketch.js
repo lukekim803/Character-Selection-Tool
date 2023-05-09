9d431baead8dc3f2da1366a6c3799f90fa469ef3
Luke Kim <105609017+lukekim803@users.noreply.github.com>
2023-05-09 18:52:56 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -82,9 +82,10 @@ function calculate()
     if(value1 == "player-flight" && value2 == "weap-gun" && value3 == "player-swift" && value4 == "player-aquamarine"){
         document.getElementById("result").innerHTML = "Raina";
     }
-       if(value1 == "player-flight" && value2 == "weap-sword" && value3 == "player-strength" && value4 == "player-orange"){
+    if(value1 == "player-flight" && value2 == "weap-sword" && value3 === "player-strength" && value4 == "player-orange"){
         document.getElementById("result").innerHTML = "Falcone";
     }
+    
     
 
     console.log(value1)
