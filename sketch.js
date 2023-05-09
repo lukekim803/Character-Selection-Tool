0238af2f07e8e2fda33f6105f27a0ea5847e0e68
Luke Kim <105609017+lukekim803@users.noreply.github.com>
2023-05-09 18:30:08 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,49 +1,92 @@
+window.onload=function(){
 
-document.getElementById("gen-btn").addEventListener("click",calculate);
+    const mainBtn = document.getElementById("gen-btn");
+    mainBtn.addEventListener("click",calculate);
+      }
+
 
 function calculate()
 {
 
-    const playerWeaponSelector = document.getElementById("player-weapon");
-    let weapchoice = playerWeaponSelector.value;
+    // const playerWeaponSelector = document.getElementById("player-weapon");
+    // let weapchoice = playerWeaponSelector.value;
 
-    var value1 = document.getElementById("player-abil").value;
-    var value2 = document.getElementById("player-weap").value;
-    var value3 = document.getElementById("player-trait").value;
-    var value4 = document.getElementById("player-eyecolor").value;
+    var value1 = document.querySelector(".player-abil").value;
+    var value2 = document.querySelector(".player-weapon").value;
+    var value3 = document.querySelector(".player-traits").value;
+    var value4 = document.querySelector(".player-eyecolor").value;
 
     //var result = value1 + " " + value2 + " " + value3 + " " + value4 + "";
 
     //document.getElementById("result").innerHTML = "Result : " + result;
 
-    if(value1 == "player-flight" || value1 == "player-morph") {
-        score++;
-    }
+    // if(value1 == "player-flight" || value1 == "player-morph") {
+        
+    // }
 
-    if(value2 == "weap-sword" || value2 == "weap-gun"){
+    // if(value2 == "weap-sword" || value2 == "weap-gun"){
 
-    }
-    if(value3 == "player-strength" || value3 == "player-swift"){
+    // }
+    // if(value3 == "player-strength" || value3 == "player-swift"){
 
-    }
-    if(value4 == "player-aquamarine" || value4 == "player-orange"){
+    // }
+    // if(value4 == "player-aquamarine" || value4 == "player-orange"){
 
-    }
-    else{
-        document.getElementById("result").innerHTML = "Something";
-    }
+    // }
+    // else{
+    //     document.getElementById("result").innerHTML = "Something";
+    // }
 
+    if(value1 == "player-flight" && value2 == "weap-sword" && value3 == "player-strength" && value4 == "player-aquamarine"){
+        document.getElementById("result").innerHTML = "Melody of the Angelic Realms";
+    }
+    if(value1 == "player-flight" && value2 == "weap-gun" && value3 == "player-swift" && value4 == "player-orange"){
+        document.getElementById("result").innerHTML = "Grim the Sigma";
+    } 
+    if(value1 == "player-flight" && value2 == "weap-gun" && value3 == "player-strength" && value4 == "player-orange"){
+        document.getElementById("result").innerHTML = "Fireborn of the Skies";
+    }
+    if(value1 == "player-flight" && value2 == "weap-gun" && value3 == "player-strength" && value4 == "player-aquamarine"){
+        document.getElementById("result").innerHTML = "Mery the Stigma";
+    }
+    if(value1 == "player-flight" && value2 == "weap-sword" && value3 == "player-swift" && value4 == "player-aquamarine"){
+        document.getElementById("result").innerHTML = "Shega";
+    }
+    if(value1 == "player-flight" && value2 == "weap-sword" && value3 == "player-swift" && value4 == "player-orange"){
+        document.getElementById("result").innerHTML = "Kiriko";
+    }
+    if(value1 == "player-heal" && value2 == "weap-sword" && value3 == "player-swift" && value4 == "player-aquamarine"){
+        document.getElementById("result").innerHTML = "Kiriko";
+    }
+    if(value1 == "player-heal" && value2 == "weap-sword" && value3 == "player-strength" && value4 == "player-aquamarine"){
+        document.getElementById("result").innerHTML = "Lifesight";
+    }
+    if(value1 == "player-heal" && value2 == "weap-gun" && value3 == "player-strength" && value4 == "player-aquamarine"){
+        document.getElementById("result").innerHTML = "Mhora";
+    }
+    if(value1 == "player-heal" && value2 == "weap-gun" && value3 == "player-strength" && value4 == "player-orange"){
+        document.getElementById("result").innerHTML = "Kiti";
+    }
+    if(value1 == "player-heal" && value2 == "weap-sword" && value3 == "player-strength" && value4 == "player-orange"){
+        document.getElementById("result").innerHTML = "Lifeline";
+    }
+    if(value1 == "player-heal" && value2 == "weap-sword" && value3 == "player-swift" && value4 == "player-orange"){
+        document.getElementById("result").innerHTML = "Cyro";
+    }
+    if(value1 == "player-heal" && value2 == "weap-gun" && value3 == "player-swift" && value4 == "player-orange"){
+        document.getElementById("result").innerHTML = "Voidwalker";
+    }
+    if(value1 == "player-heal" && value2 == "weap-gun" && value3 == "player-swift" && value4 == "player-aquamarine"){
+        document.getElementById("result").innerHTML = "Pailos";
+    }
+    if(value1 == "player-flight" && value2 == "weap-gun" && value3 == "player-swift" && value4 == "player-aquamarine"){
+        document.getElementById("result").innerHTML = "Raina";
+    }
     
 
-
+    console.log(value1)
+    console.log(value2)
+    console.log(value3)
+    console.log(value4)
 
 }
-
-
-
-
-
-
-
-
-  
